e6db07b3710dfa9c3767603d42a4cb081c0afe6d
Joshua Yuter <jyuter@gmail.com>
2024-06-27 18:33:08 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,48 +15,48 @@ dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-rel
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # Install Shell Commands
-dnf install zsh
+dnf install zsh -y
 sudo -u jyuter chsh -s $(which zsh)
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
-dnf install util-linux
-dnf install alacritty
-dnf install htop
-dnf install neofetch
-dnf install neovim
-dnf install fzf
-dnf install ripgrep
-dnf install bat
-dnf install exa
+dnf install util-linux -y
+dnf install alacritty -y
+dnf install htop -y
+dnf install neofetch -y
+dnf install neovim -y
+dnf install fzf -y
+dnf install ripgrep -y
+dnf install bat -y
+dnf install exa -y
 
 # Install Programming Languages
-dnf install dotnet-sdk-8.0
-dnf install gcc
-dnf install elixir
-dnf install php-cli phpunit composer
-dnf install erlang
+dnf install dotnet-sdk-8.0 -y
+dnf install gcc -y
+dnf install elixir -y
+dnf install php-cli phpunit composer -y
+dnf install erlang -y
 
-dnf install ruby
-dnf install rubygem-{irb,rake,rbs,rexml,typeprof,test-unit} ruby-bundled-gems
+dnf install ruby -y
+dnf install rubygem-{irb,rake,rbs,rexml,typeprof,test-unit} ruby-bundled-gems -y
 
-dnf install rustup
+dnf install rustup -y
 
-dnf install golang
+dnf install golang -y
 mkdir -p $HOME/go
 echo 'export GOPATH=$HOME/go' >> $HOME/.bashrc
 source $HOME/.bashrc
 
-dnf install nodejs
+dnf install nodejs -y
 npm install -g npm@latest
 npm install -g pnpm
 npm install -g bun 
 
 # Install Containers
-dnf install podman
+dnf install podman -y
 
-dnf install dnf-plugins-core
+dnf install dnf-plugins-core -y
 dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo 
-dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 systemctl start docker
 systemctl enable docker
@@ -65,15 +65,15 @@ systemctl enable docker
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf check-update
-dnf install code
+dnf install code -y
 
 # Install Chrome
-dnf install fedora-workstation-repositories
+dnf install fedora-workstation-repositories -y
 dnf config-manager --set-enabled google-chrome 
-dnf install google-chrome-stable
+dnf install google-chrome-stable -y
 
 # Install Media
-dnf install vlc
+dnf install vlc -y
 
 # Install Flatpacks
 flatpak install flathub com.todoist.Todoist -y
